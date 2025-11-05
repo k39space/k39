31f7c2f5c30d1dee33c455ed5d5e771ecef13ee6
Nick Kosarev <hmbanan666@hotmail.com>
2025-11-05 11:04:56 +0200

    feat: new pages

---
+++ apps/web-app/app/pages/[pageSlug]/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <UContainer>
+    <UBreadcrumb :items="breadcrumbItems" class="mt-4" />
+
+    {{ params.pageSlug }}
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import type { BreadcrumbItem } from '@nuxt/ui'
+
+const { params } = useRoute('pageSlug___ru')
+
+const breadcrumbItems = computed<BreadcrumbItem[]>(() => [
+  {
+    label: 'Главная',
+    icon: 'i-lucide-house',
+    to: '/',
+  },
+  {
+    label: 'Страница',
+    icon: 'i-lucide-map',
+  },
+])
+
+useHead({
+  title: params.pageSlug,
+})
+</script>
